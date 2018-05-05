e026b900f113996209f7e13a8760ca932dec43b0
Viordik <fataliev2009@rambler.ru>
2018-05-05 13:03:41 +0300

    Добавлен скрипт для ЯКарты

--- js/index.js
+++ js/index.js
@@ -6,3 +6,19 @@ btnMenu.addEventListener('click', function () {
   menu.classList.toggle('menu-nav--active');
 })
 
+//===============================================
+// Yandex Map
+
+ymaps.ready(init);
+var myMap;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [55.709622, 37.624885],
+        zoom: 16
+    });
+    myMap.behaviors.disable('scrollZoom');
+    myPlacemark = new ymaps.Placemark([55.709622, 37.624885], { hintContent: 'ГБЭС', balloonContent: 'Холодильный переулок д.3, корп.1, оф. 4402' });
+
+    myMap.geoObjects.add(myPlacemark);
+}
