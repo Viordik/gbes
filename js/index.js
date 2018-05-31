34b3d2978b09e96584541e9f57613050c964f315
Viordik <fataliev2009@rambler.ru>
2018-05-31 15:35:49 +0300

    Внесены правки и добавлен js код

--- js/index.js
+++ js/index.js
@@ -5,12 +5,31 @@ var menu = document.querySelector('.menu-nav');
 btnMenu.addEventListener('click', function () {
   menu.classList.toggle('menu-nav--active');
 });
+//===============================================
+//Активный пункт меню
 
 //===============================================
-//Раскрывающиеся блоки с историей компании на странице О компании
-var btnYear = document.querySelectorAll('.history-company__btn-year');
+//Раскрывающиеся блоки с историей компании на странице "О компании"
+var yearButtons = document.querySelectorAll('.history-company__btn-year');
 var blockHide = document.querySelectorAll('.history-company__text-hide');
+var lotHouse = document.querySelectorAll('.lot-house__description-hide');
+var news = document.querySelectorAll('.text-hide')
 
+console.log(lotHouse);
+
+Array.prototype.forEach.call(yearButtons, function(yearButton, index) {
+  yearButton.addEventListener('click', function(evt) {
+
+      blockHide[index].classList.toggle('history-company__text-hide--active');
+
+      // lotHouse[index].classList.toggle('lot-house__description-hide--active');
+
+      // news[index].classList.toggle('text-hide--active');
+
+  })
+});
+
+//===============================================
 
 //===============================================
 // Yandex Map
