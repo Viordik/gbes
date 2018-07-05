3966477ee3b1fdf03c8a45d88fb4a22123b3b3c5
Viordik <fataliev2009@rambler.ru>
2018-07-05 22:32:17 +0300

    Добавлен код для раскрывания текста на страницах О компании, Новости, Торги

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-var btnMenu = document.querySelector('.menu__btn');
-var menu = document.querySelector('.menu-nav');
-
+const btnMenu = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu-nav');
 
 btnMenu.addEventListener('click', function () {
   menu.classList.toggle('menu-nav--active');
@@ -10,8 +9,8 @@ btnMenu.addEventListener('click', function () {
 
 //===============================================
 //Выпадающее под меню
-var secondMenu = document.querySelector('.menu-list__dropdown');
-var open = document.querySelector('.open');
+const secondMenu = document.querySelector('.menu-list__dropdown');
+const open = document.querySelector('.open');
 
 open.addEventListener('click', function () {
   open.classList.toggle('open--active');
@@ -20,21 +19,24 @@ open.addEventListener('click', function () {
 
 //===============================================
 //Раскрывающиеся блоки с историей компании на странице "О компании"
-var yearButtons = document.querySelectorAll('.history-company__btn-year');
-var blockHide = document.querySelectorAll('.history-company__text-hide');
-var lotHouse = document.querySelectorAll('.lot-house__description-hide');
-var news = document.querySelectorAll('.text-hide')
+const yearButtons = document.querySelectorAll('.history-company__btn-year');
+const blockHide = document.querySelectorAll('.history-company__text-hide');
+const lotHouse = document.querySelectorAll('.lot-house__description-hide');
+const news = document.querySelectorAll('.text-hide')
 
 console.log(lotHouse);
 
 Array.prototype.forEach.call(yearButtons, function(yearButton, index) {
   yearButton.addEventListener('click', function() {
 
-      blockHide[index].classList.toggle('history-company__text-hide--active');
+      if (blockHide.length > 0)
+        blockHide[index].classList.toggle('history-company__text-hide--active');
 
-      // lotHouse[index].classList.toggle('lot-house__description-hide--active');
+      if (lotHouse.length > 0)
+        lotHouse[index].classList.toggle('lot-house__description-hide--active');
 
-      // news[index].classList.toggle('text-hide--active');
+      if (news.length > 0)
+        news[index].classList.toggle('text-hide--active');
 
   })
 });
@@ -42,56 +44,63 @@ Array.prototype.forEach.call(yearButtons, function(yearButton, index) {
 //===============================================
 // Yandex Map
 
-ymaps.ready(init);
-var myMap;
+if (document.querySelector('#map')) {
+  ymaps.ready(init);
+  var myMap;
 
-function init(){
-    myMap = new ymaps.Map("map", {
-        center: [55.709622, 37.624885],
-        zoom: 16
-    });
-    myMap.behaviors.disable('scrollZoom');
-    myMap.behaviors.disable('drag');
-    myPlacemark = new ymaps.Placemark([55.709622, 37.624885], { hintContent: 'ГБЭС', balloonContent: 'Холодильный переулок д.3, корп.1, оф. 4402' });
+  function init(){
+      myMap = new ymaps.Map("map", {
+          center: [55.709622, 37.624885],
+          zoom: 16
+      });
+      myMap.behaviors.disable('scrollZoom');
+      myMap.behaviors.disable('drag');
+      myPlacemark = new ymaps.Placemark([55.709622, 37.624885], { hintContent: 'ГБЭС', balloonContent: 'Холодильный переулок д.3, корп.1, оф. 4402' });
 
-    myMap.geoObjects.add(myPlacemark);
+      myMap.geoObjects.add(myPlacemark);
+  }
 }
 
-//===============================================
-// Карусуль на главной странице
-$(document).ready(function(){
-  $('.services').slick({
-    autoplay: true,
-    autoplaySpeed: 1900,
-    dots: true,
-    infinite: true,
-    speed: 900,
-    slidesToShow: 1,
-  });
-});
+;(function() {
+    //===============================================
+    // Карусуль на главной странице
 
-$(document).ready(function(){
-  $('.our-clients__wrapper').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    infinite: true,
-    dots: true,
-    variableWidth: true,
-  });
-});
+      $(document).ready(function(){
+        $('.services').slick({
+          autoplay: true,
+          autoplaySpeed: 1900,
+          dots: true,
+          infinite: true,
+          speed: 900,
+          slidesToShow: 1,
+        });
+      });
 
-// Карусель на странице Лот
-$(document).ready(function(){
-  $('.lot-img').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    infinite: true,
-    dots: true,
-    variableWidth: true,
-  });
-});
-//================================================
+      $(document).ready(function(){
+        $('.our-clients__wrapper').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          infinite: true,
+          dots: true,
+          variableWidth: true,
+        });
+      });
+
+      // Карусель на странице Лот
+      $(document).ready(function(){
+        $('.lot-img').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          infinite: true,
+          dots: true,
+          variableWidth: true,
+        });
+      });
+      //================================================
+
+})();
+
